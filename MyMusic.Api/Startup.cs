6b27d0997b092c293bb074e3705dcb3ef00a42cd
avelarder-kz <arturo.velarde@kzsoftworks.com>
2021-07-05 09:50:41 -0300

    Removing unnecessary files

--- MyMusic.Api/Startup.cs
+++ MyMusic.Api/Startup.cs
@@ -52,7 +52,7 @@ namespace MyMusic.Api
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
-                    c.SwaggerEndpoint("swagger/v1/swagger.json", "MyMusic.Api v1");
+                    c.SwaggerEndpoint("v1/swagger.json", "MyMusic.Api v1");
                     c.RoutePrefix = "swagger";
                 });
             }
